72a0e94b91ea8c48eb7d78971ca5f6a62cced449
Emiliobocadillo <82929546+Emiliobocadillo@users.noreply.github.com>
2024-04-20 09:57:16 +0200

    First commit

---
+++ src/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Your Submenu System <br /> Implemented
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          recusandae inventore asperiores minus.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Hero;
